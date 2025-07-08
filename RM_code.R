575954b613ac1c37691b262fbd179075e06b8439
Chengxu Tong <43085757+tu-xuu@users.noreply.github.com>
2025-07-08 17:01:16 +0100

    Update RM_code.R

--- RM_code.R
+++ RM_code.R
@@ -1,45 +1,38 @@
 RM_method=function(pollutant,startdate,enddate,mydata,data_MET,samples_time,sitename){
-  pollutant=pollutant
-  startdate=startdate
-  enddate=enddate
-  mydata=mydata
-  sitename=sitename
-  data_MET=data_MET
-  samples_time=samples_time
-  library(magrittr)
-  print(paste(sitename,'is beginning', sep=' '))
-  workingDirectory<<-"./"  ### Shortcut for the working directory
-  model_start_time <- Sys.time()
   
+  message(paste0("Running RM_method for site: ", sitename))
+  workingDirectory <<- "./"  # Shortcut for internal path use
+  model_start_time <- Sys.time()
+
+  # Define columns used in the model
   mycols=c('date',
     "date_unix","day_julian", "weekday", "hour", 
     "temp",  "RH", "ws","wd",'ssr','tp','blh','tcc','sp')
- 
-  
-  mydata=mydata%>%select(c(mycols,pollutant
-                           # ,'cluster'
-                           ))
 
-  data_prepared <- mydata%>% 
-    filter(!is.na(ws)) %>% 
-    dplyr::rename(value = pollutant) %>% 
+
+  data_prepared <- mydata %>%
+    dplyr::select(all_of(c(model_columns, pollutant))) %>%
+    dplyr::rename(value = all_of(pollutant)) %>%
+    dplyr::filter(!is.na(ws)) %>%
     rmw_prepare_data(na.rm = TRUE)
   
+  # Print model training info
   set.seed(12345) 
-  print(paste('The model is building between',startdate,'to',enddate,sep=' '))
-  ## Build the model
+  message(paste0("Building model from ", startdate, " to ", enddate))
+  
+  # Train the meteorologically-normalized model using random forest
   RF_model <- rmw_do_all(
     data_prepared%>%selectByDate(start=startdate,end=enddate),
     variables = c(
       "date_unix","day_julian", "weekday", "hour", 
-      "temp",  "RH", "ws","wd",'ssr','tp','blh','tcc','sp'
-      # , 'cluster'
+      "temp",  "RH", "ws","wd",'ssr','tp','blh','tcc','sp'     
       ),
     n_trees = 300,
     n_samples = samples_time,
     verbose = TRUE
   )
   
+  # Evaluate model performance on the test set
   rr=modStats(rmw_predict_the_test_set(model = RF_model$model,df = RF_model$observations) , 
               mod = "value", obs = "value_predict", 
               statistic = c("n", "FAC2","MB", "MGE", "NMB", "NMGE", "RMSE", "r","COE", "IOA"),
@@ -47,24 +40,25 @@ RM_method=function(pollutant,startdate,enddate,mydata,data_MET,samples_time,site
   
   print(paste('Model is ready, Your model R square is:', round(rr, 2), sep=' ' ))
   
-  data_MET $date_unix  <- as.numeric(data_MET $date)
-  data_MET $week <-lubridate:: week(data_MET$date)
-  data_MET $weekday <- weekdays(data_MET $date, abbreviate = TRUE) 
-  data_MET $weekday[which(data_MET $weekday == "Mon")] = 1
-  data_MET $weekday[which(data_MET $weekday == "Tue")] = 2
-  data_MET $weekday[which(data_MET $weekday == "Wed")] = 3
-  data_MET $weekday[which(data_MET $weekday == "Thu")] = 4
-  data_MET $weekday[which(data_MET $weekday == "Fri")] = 5
-  data_MET $weekday[which(data_MET $weekday == "Sat")] = 6
-  data_MET $weekday[which(data_MET $weekday == "Sun")] = 7
-  data_MET $hour <- lubridate::hour(data_MET $date)
-  data_MET $month <- lubridate::month(data_MET $date)
-  data_MET $day_julian <- lubridate::yday(data_MET $date)
+### ==== Step 2: Prepare MET Resample Base ==== ###
   
-  data_MET =data_MET %>%select(mycols)
-  # data_MET$cluster=NA
+data_MET <- data_MET %>%
+    mutate(
+      date_unix = as.numeric(date),
+      week = lubridate::week(date),
+      weekday = weekdays(date, abbreviate = TRUE),
+      hour = lubridate::hour(date),
+      month = lubridate::month(date),
+      day_julian = lubridate::yday(date)
+    ) %>%
+    mutate(
+      weekday = recode(weekday,
+                       "Mon" = 1, "Tue" = 2, "Wed" = 3, "Thu" = 4,
+                       "Fri" = 5, "Sat" = 6, "Sun" = 7)
+    ) %>%
+    dplyr::select(all_of(model_columns))
   
-  
+  # Extract matched observation set to apply resampled weather
   re_sample_MET<-mydata%>%
     filter(date >= startdate & date <= enddate)
   # Reorder columns in re_sample_MET to match the order of data_MET
@@ -73,7 +67,7 @@ RM_method=function(pollutant,startdate,enddate,mydata,data_MET,samples_time,site
   
   print(paste('Your ',setdiff(names(re_sample_MET), names(data_MET)), ' corresponding Met data base is ready',sep=''))
   
-  
+   ### ==== Step 3: Define Weather Generator ==== ###
   pred<- mydata %>% select(1)
   
   # Filter the dataframe to select rows within the specified date range
@@ -82,7 +76,7 @@ RM_method=function(pollutant,startdate,enddate,mydata,data_MET,samples_time,site
   new_met<- data_MET %>%  slice(0)
   
   ##Function to generated the new- weather from original weather
-  ## This algorithm will remove the effects of shor impact of weather between each year, but not normlaise the seasonal impact in each year. You can you theisel to remove. 
+ 
   new_met<-function (i) {
     hour_1 <- data_MET[i, "hour"]
     day_1 <- data_MET[i, "day_julian"] 
@@ -95,6 +89,7 @@ RM_method=function(pollutant,startdate,enddate,mydata,data_MET,samples_time,site
     new_met<-MET_sample
     return (new_met)}
   
+   ### ==== Step 4: Define Prediction Function ==== ###
   #Function to predict the concentration of a pollutant using a re-sampled weather        
   set.seed(12345)
   cols_to_replace <- c("ws", "wd", "temp", "RH", "ssr", "tp", "blh", "tcc", "sp")
@@ -112,14 +107,15 @@ RM_method=function(pollutant,startdate,enddate,mydata,data_MET,samples_time,site
   
    print(paste('Model is replacing, Your resample times is ',samples_time,sep=''))
    
-   
-   
-   final_weather_normalised <- prediction (samples_time)  ## Randomly ran for 150 predictions. Increase the number of prediction until it is stable
+   ### ==== Step 5: Generate Final Normalized Data ==== ###
+  
+   ## Randomly ran for 150 predictions. Increase the number of prediction until it is stable
+   final_weather_normalised <- prediction (samples_time)  
 
-   
   cols_number=samples_time+1
   final_weather_normalised$final <- apply(final_weather_normalised[,2:cols_number],1,mean, na.rm=TRUE) ### Mean value of 150 predictions
   
+  ### ==== Step 6: Save Results ==== ###
   ### File names 
   filenames1=paste(workingDirectory,"WN_data_", pollutant,'_',startdate,'_',enddate,'_',sitename,".csv",sep="")
   filenames2=paste(workingDirectory,"Tuan_", pollutant,'_',startdate,'_',enddate,'_',sitename,"_results.csv",sep="")
@@ -127,7 +123,7 @@ RM_method=function(pollutant,startdate,enddate,mydata,data_MET,samples_time,site
   ### Save the prediction
   write.csv(final_weather_normalised,filenames1) ### Save all predictions
   
-  ### COMPARiSON obseved and nomarlised concentration. May correct with testing data.
+  # Compare observed vs normalized concentrations
   data_compare<-merge(mydata,final_weather_normalised, by="date") 
   
   data_compare <- data_compare %>% dplyr:: rename(WN_data=final,Observed_data=pollutant) %>%
@@ -139,7 +135,7 @@ RM_method=function(pollutant,startdate,enddate,mydata,data_MET,samples_time,site
            ylab=expression("Pollutant concentration"* " (" * mu * "g m" ^-3 * ")"))
   
   
-  write.csv(data_compare,filenames2) ### Save 1000 predictions 
+  write.csv(data_compare,filenames2) 
   save.image(Rfilename)
   
   
@@ -147,7 +143,7 @@ RM_method=function(pollutant,startdate,enddate,mydata,data_MET,samples_time,site
   
   # Calculate time difference
   time_used <- model_end_time - model_start_time
-  print(time_used)
+  message(paste0("Completed. Time used: ", time_used))
   
   return(p)
   
