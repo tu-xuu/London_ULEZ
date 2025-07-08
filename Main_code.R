e1eb519f3c7eb5a5e2c0d990c9a5ff3b296a2b58
Chengxu Tong <43085757+tu-xuu@users.noreply.github.com>
2025-07-08 16:06:40 +0100

    Update Main_code.R

--- Main_code.R
+++ Main_code.R
@@ -1,27 +1,17 @@
 ##############################################
-######The main code to run Tuan Method#######
+######The main code to run WN#################
 ##############################################
-rm(list=ls())
-setwd('~/shiz-shi-aphh/Tuan_results/')
 
-pollutant='NO2'
-##### The pollutant, NO2 and PM2.5
+#rm(list=ls())
+setwd('YOUR WD')
 
-samples_time=140
-#### Resample times
-startdate <- as.POSIXct('2018-03-08') # 3 years or 20181001-20191031
-enddate <- as.POSIXct('2020-03-08') # 3 years or 20181001-20191031
-### The model will be trained in this time period
+##### Load Environment #####
 
-
-workingDirectory<<-"./"  ### Shortcut for the working directory
-
-##### load envirnment#####
-# List of packages
+# List of required R packages
 packages <- c("openair", "plyr", "dplyr", "rmweather", "lubridate", "magrittr",
               "tibble", "janitor", "RCurl", "rio", "stringr")
 
-# Function to check if a package is available and install it if necessary
+# Function to check if a package is installed; install it if not, then load it
 check_and_load_package <- function(package_name) {
   if (!require(package_name, character.only = TRUE)) {
     install.packages(package_name)
@@ -31,67 +21,75 @@ check_and_load_package <- function(package_name) {
   }
 }
 
-# Check and load each package
+# Load all required packages
 for (package in packages) {
   check_and_load_package(package)
 }
 
-source('./Time_processing.R') # Modified the date column
-source('./Tuan_method.R') # The function to run the Tuan's method
+# Load custom functions for time processing and normalization method
+source("./Time_processing.R") 
+source("./RM_code.R") 
+
+##### Parameter Settings #####
+
+# Specify the target pollutant: options include "NO2" or "PM2.5"... (case-sensitive)
+pollutant <- "NO2"
+
+# Number of resampling iterations used in the meteorological normalization process
+samples_time <- 150
+
+# Define the time period for model training
+startdate <- as.POSIXct("2018-03-08") 
+enddate   <- as.POSIXct("2020-03-08")
 
 
+# List of site names to be analyzed; each name should match the corresponding CSV file name
+sitenames <- c("AQ_MET_traj_London_Avg1_UT")
+# Add more site names as needed
 
-sitenames=c('AQ_MET_traj_London_Avg1_UT'
-           ) 
-### Add more site names
+# Load meteorological data and apply time preprocessing
+# Customize the time window (e.g., 5 years, 10 years) for resampling as needed
+data_MET <- read.csv("./London_MET_2000_2024.csv") %>% Time_processing()
+
+#### Loop through each site and run the meteorological normalization pipeline ####
 
 for (sitename in sitenames) {
-  setwd('~/shiz-shi-aphh/Tuan_results/')
-  # print(paste(sitename,'is starting',sep=' '))
   
-  dirname=paste('./',sitename,sep='')
+  # Set the base working directory — replace 'YOUR WD' with your actual project path or use `here::here()` for safety
+  setwd("YOUR WD")  
   
-  # Check if the directory exists
+  # Define a folder for the current site
+  dirname <- paste0("./", sitename)
+  
+  # Create the directory if it doesn't exist
   if (!dir.exists(dirname)) {
-    # If the directory doesn't exist, create it
     dir.create(dirname)
   } else {
-    # If the directory exists, do nothing or print a message
-    # You can customize this part according to your needs
-    print(paste("Directory", dirname, "already exists."))
+    message(paste("Directory", dirname, "already exists."))
   }
   
+  # Switch to the site's working directory
   setwd(dirname)
+  workingDirectory <<- "./"  # Shortcut for internal function use
   
+  # Load air quality data for the site and preprocess it
+  mydata <- read.csv(paste0("../", sitename, ".csv")) %>% Time_processing()
   
-  workingDirectory<<-"./"  ### Shortcut for the working directory
-  
-  
-  
-  #########
-  mydata<-read.csv(paste("../",sitename,".csv",sep="")) %>%Time_processing()
-  ##### The data with AQ to build a Model
-
-  ### Get ready the MET database
-  data_MET <-mydata%>%
-    selectByDate(year=2017:2022)
-  
-  
-  
-  Tuan_method_result_figure=Tuan_method(
-    sitename=sitename,
-    pollutant=pollutant,
-    startdate=startdate,
-    enddate=enddate,
-    mydata=mydata,
-    data_MET=data_MET,
-    samples_time = samples_time
+  # Run the RM (meteorological normalization) method
+  WN_result_figure <- RM_method(
+    sitename      = sitename,
+    pollutant     = pollutant,
+    startdate     = startdate,
+    enddate       = enddate,
+    mydata        = mydata,
+    data_MET      = data_MET,
+    samples_time  = samples_time
   )
-  ##### The function will write three files in the current working directory:
-  #### 1. Rdata saves all the info we need.
-  #### 2. For every resample result, for example, if the resample times is 150, it will contain all 150 times results.
-  #### 3. The final result is the Observation data and final WN data.
-  #### The function will output a figure at the plot panel, which you can have a quick check.
-  
+
+  # RM_method will output:
+  # 1. An `.RData` file storing all model objects and results
+  # 2. A `.csv` file containing all resampling results (e.g. 150 iterations)
+  # 3. A `.csv` file with observed vs normalized pollutant concentrations
+  # 4. A visualization plot rendered in the R plot window
   
 }
